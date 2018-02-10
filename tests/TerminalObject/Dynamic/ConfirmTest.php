ca11b19d1adc801643ccb779f2c39ef2ab3cbbb3
Craig Duncan <git@duncanc.co.uk>
2018-02-10 20:42:05 +0000

    Organise the tests into folders matching the classes they test

---
+++ tests/TerminalObject/Dynamic/ConfirmTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace League\CLImate\Tests\TerminalObject\Dynamic;
+
+use League\CLImate\Tests\TestBase;
+
+class ConfirmTest extends TestBase
+{
+    /** @test */
+    public function it_will_return_true_for_y()
+    {
+        $this->shouldReadAndReturn('y');
+        $this->shouldReceiveSameLine();
+        $this->shouldWrite("\e[mKeep going? [y/n] \e[0m");
+
+        $input = $this->cli->confirm('Keep going?', $this->reader);
+
+        $response = $input->confirmed();
+
+        $this->assertTrue($response);
+    }
+
+    /** @test */
+    public function it_will_return_false_for_n()
+    {
+        $this->shouldReadAndReturn('n');
+        $this->shouldReceiveSameLine();
+        $this->shouldWrite("\e[mKeep going? [y/n] \e[0m");
+
+        $input = $this->cli->confirm('Keep going?', $this->reader);
+
+        $response = $input->confirmed();
+
+        $this->assertFalse($response);
+    }
+
+    /** @test */
+    public function it_will_only_allow_strict_confirmations()
+    {
+        $this->shouldReadAndReturn('Y');
+        $this->shouldReadAndReturn('y');
+        $this->shouldReceiveSameLine();
+        $this->shouldWrite("\e[mKeep going? [y/n] \e[0m", 2);
+
+        $input = $this->cli->confirm('Keep going?', $this->reader);
+
+        $input->confirmed();
+    }
+}
